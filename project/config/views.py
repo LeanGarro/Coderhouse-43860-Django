4dda5529c7bb703a2b45e11c14ac00c3af3adc70
Leandro Garro <leandro garro>
2023-06-27 20:51:59 -0300

    terminado 2 templates y creando app

--- project/config/views.py
+++ project/config/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
-from django.template import Context, Template
+from django.shortcuts import render
+from datetime import datetime
 
 
 def saludo(request):
@@ -15,11 +16,26 @@ def nombre(request, nombre: str, apellido: str):
 
 def probando_template(request):
 	mi_html = open("./templates/template1.html")
-	mi_template = Template(mi_html.read())
+	#mi_template = Template(mi_html.read())
 	mi_html.close()
-	mi_contexto = Context()
-	mi_documento = mi_template.render(mi_contexto)
-	return HttpResponse(mi_documento)
+	nombre = "Luis" #! nuevo
+	apellido = "Van Beethoven" #! nuevo
+	datos = {"nombre": nombre, "apellido": apellido,} #! nuevo
+	datos["fecha"]: ahora
+	#mi_contexto = Context(datos) #! cambios
+	#mi_documento = mi_template.render(mi_contexto)
+	return render(request, "template1.html", context=datos)
 
+def ahora(request):
+	ahora = datetime.now().strftime("%d/%m/%Y %H:%M:S.%f")
+	return HttpResponse(f"<h1> Feche y Hora: {ahora} </h1>")
 
+def mis_notas(request):
+	lista = [2, 3, 4, 5, 7, 8, 9, 10]
+	contexto = {"notas": lista}
+	return render(request, "notas.html", contexto)
 
+def amigos(request):
+	amigos = ["juan", "nahuel", "sarmiento", "jose"]
+	contexto = {"amigos": amigos}
+	return render(request, "nuevo.html", contexto)
