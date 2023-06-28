fef7b28f2f5de0d0ee360ea4a71fc6494aec759b
Leandro Garro <leandro garro>
2023-06-27 21:13:06 -0300

    base de datos creada

--- project/cliente/models.py
+++ project/cliente/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class pais(models.Model):
+    nombre= models.CharField(max_length=50)
+    
+    def __str__(self) -> str:
+        return self.nombre
+
+class cliente(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    nacimiento = models.DateField(null=True)
+    pais_de_origen = models.ForeignKey(pais, on_delete=models.SET_NULL, null=True)
+    
+    def __str__(self) -> str:
+        return f"{self.nombre} {self.apellido}"
+    
